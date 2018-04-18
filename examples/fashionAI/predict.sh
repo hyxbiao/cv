7876209c57b3fc2849c6758fe41009ba69bbdb39
xuanbiao <xuanbiao@baidu.com>
2018-04-18 22:27:47 +0800

    update script

--- examples/fashionAI/predict.sh
+++ examples/fashionAI/predict.sh
@@ -1,6 +1,7 @@
 
-attr_key=${1:-skirt_length_labels}
-model_dir=$2
+data_dir=${1:-$HOME/data/vision/fashionAI/}
+attr_key=${2:-skirt_length_labels}
+model_dir=$3
 
 now=`date +%Y%m%d` 
 #output_file=output/$now/${attr_key}.csv
@@ -8,12 +9,13 @@ now=`date +%Y%m%d`
 output_dir=output/$now/${attr_key}
 mkdir -p $output_dir
 
+echo "data_dir: $data_dir"
 echo "attr_key: $attr_key"
 echo "model_dir: $model_dir"
 echo "output_dir: $output_dir"
 
 python app.py \
-	--data_dir ~/data/vision/fashionAI/ \
+	--data_dir $data_dir \
 	--model_dir $model_dir \
 	--attr_key $attr_key \
 	--predict \
--- examples/fashionAI/train.sh
+++ examples/fashionAI/train.sh
@@ -1,39 +1,36 @@
 
-attr_key=${1:-skirt_length_labels}
-mode=${2:-train}
-debug=$3
+data_dir=${1:-$HOME/data/vision/fashionAI/}
+attr_key=${2:-skirt_length_labels}
+pretrain_model_dir=${3:-models/pretrain_resnet50/}
 
 base_model_dir=models/$attr_key
 mkdir -p $base_model_dir
 
-now=`date +%Y%m%d_%H%M%S` 
+now=`date +%Y%m%d_%H%M` 
 model_dir=$base_model_dir/$now
 
 #pretrain_model_dir=models/skirt_length_labels/baseline/
 pretrain_model_dir=models/pretrain_resnet50/
 pretrain_warm_vars='^((?!dense).)*$'
-if [ "$attr_key" = "skirt_length_labels" ]; then
-	pretrain_warm_vars='.*'
-	pretrain_model_dir=model_skirt_pretrain/
-fi
+
+#if [ "$attr_key" = "skirt_length_labels" ]; then
+#	pretrain_warm_vars='.*'
+#	pretrain_model_dir=model_skirt_pretrain/
+#fi
 
 mkdir log
 
-if [ "$mode" = "predict" ]; then
-	train_or_predict="--predict"
-fi
 
-echo "mode: $mode"
+echo "data_dir: $data_dir"
 echo "attr_key: $attr_key"
 echo "model_dir: $model_dir"
 echo "pretrain_model_dir: $pretrain_model_dir"
 echo "pretrain_warm_vars: $pretrain_warm_vars"
 
 nohup python app.py \
-	--data_dir ~/data/vision/fashionAI/ \
+	--data_dir $data_dir \
 	--model_dir $model_dir \
 	--attr_key $attr_key \
 	--pretrain_model_dir $pretrain_model_dir \
 	--pretrain_warm_vars $pretrain_warm_vars \
-	$train_or_predict \
 	>log/${attr_key}_${now}.out 2>&1 &
